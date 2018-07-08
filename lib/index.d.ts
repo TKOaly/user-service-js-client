32628f68b9452e5cac0df37024183ecaea586935
alehuo <aleksi.huotala@helsinki.fi>
2018-07-08 16:16:46 +0300

    Update tsconfig to autogenerate d.ts file.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,24 +1,29 @@
-declare module "user-service-js-client" {
-  enum Service {
+export declare enum Service {
     KJYR = "433f7cd9-e7db-42fb-aceb-c3716c6ef2b7",
     EVENT_CALENDAR = "65a0058d-f9da-4e76-a00a-6013300cab5f"
-  }
-
-  interface AuthenticationResult {
+}
+export interface AuthenticationResult {
+    ok?: boolean;
+    message?: string;
+    payload: TokenPayload;
+}
+export interface TokenPayload {
     token?: string;
-    error?: string;
-    success?: boolean;
-  }
-
-  interface ClientOptions {
+}
+export interface ClientOptions {
     baseURL?: string;
     timeout?: number;
-  }
-
-  function authenticate(
-    username: string,
-    password: string,
-    serviceIdentifier: Service | string,
-    options?: ClientOptions
-  ): Promise<AuthenticationResult>;
 }
+export interface AuthenticationModel {
+    token?: string;
+    success: boolean;
+    error?: string;
+}
+declare function authenticate(username: string, password: string, serviceIdentifier: Service | string, options?: ClientOptions): Promise<AuthenticationModel>;
+declare function getMyData(token: string, serviceIdentifier: Service | string, options?: ClientOptions): Promise<void>;
+declare const _default: {
+    authenticate: typeof authenticate;
+    getMyData: typeof getMyData;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,118 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const user_service_url: string = "http://localhost:8080";
 const auth_endpoint: string = "/api/auth/authenticate";
+const user_endpoint: string = "/api/users/me";
 
+/**
+ * Service enums.
+ *
+ * @export
+ * @enum {number}
+ */
 export enum Service {
   KJYR = "433f7cd9-e7db-42fb-aceb-c3716c6ef2b7",
   EVENT_CALENDAR = "65a0058d-f9da-4e76-a00a-6013300cab5f"
 }
 
+/**
+ * Authentication result interface.
+ *
+ * @export
+ * @interface AuthenticationResult
+ */
 export interface AuthenticationResult {
-  token?: string;
-  error?: string;
-  success?: boolean;
+  /**
+   * True if the request succeeds.
+   *
+   * @type {boolean}
+   * @memberof AuthenticationResult
+   */
+  ok?: boolean;
+  /**
+   * Response message.
+   *
+   * @type {string}
+   * @memberof AuthenticationResult
+   */
+  message?: string;
+  /**
+   * Token payload.
+   *
+   * @type {TokenPayload}
+   * @memberof AuthenticationResult
+   */
+  payload: TokenPayload;
 }
 
+/**
+ * Token payload.
+ *
+ * @export
+ * @interface TokenPayload
+ */
+export interface TokenPayload {
+  /**
+   * JWT.
+   *
+   * @type {string}
+   * @memberof TokenPayload
+   */
+  token?: string;
+}
+
+/**
+ * Client options.
+ *
+ * @export
+ * @interface ClientOptions
+ */
 export interface ClientOptions {
+  /**
+   * Base URL of the request.
+   *
+   * @type {string}
+   * @memberof ClientOptions
+   */
   baseURL?: string;
+  /**
+   * Request timeout.
+   *
+   * @type {number}
+   * @memberof ClientOptions
+   */
   timeout?: number;
 }
 
-const authenticate = async (
+/**
+ * Authentication model.
+ *
+ * @export
+ * @interface AuthenticationModel
+ */
+export interface AuthenticationModel {
+  token?: string;
+  success: boolean;
+  error?: string;
+}
+
+/**
+ * Authenticates the user.
+ *
+ * @param {string} username Username
+ * @param {string} password Password
+ * @param {(Service | string)} serviceIdentifier Service idenfitier
+ * @param {ClientOptions} [options] Request options
+ * @returns {Promise<AuthenticationModel>} Authentication model
+ */
+async function authenticate(
   username: string,
   password: string,
   serviceIdentifier: Service | string,
   options?: ClientOptions
-): Promise<AuthenticationResult> => {
-  let authRes: AuthenticationResult;
-
+): Promise<AuthenticationModel> {
   try {
-    const res = await axios
+    const res: AxiosResponse<AuthenticationResult> = await axios
       .create({
         baseURL:
           options && options.baseURL ? options.baseURL : user_service_url,
@@ -40,22 +124,35 @@ const authenticate = async (
         serviceIdentifier
       });
 
-    authRes = {
+    return {
       token: res.data.payload.token,
       success: true
-    };
-
-    return authRes;
+    } as AuthenticationModel;
   } catch (err) {
-    authRes = {
+    throw {
       success: false,
       error: err.response
-    };
-
-    throw authRes;
+    } as AuthenticationModel;
   }
-};
+}
+
+async function getMyData(
+  token: string,
+  serviceIdentifier: Service | string,
+  options?: ClientOptions
+) {
+  const res: AxiosResponse<any> = await axios
+    .create({
+      baseURL: options && options.baseURL ? options.baseURL : user_service_url,
+      timeout: options && options.timeout ? options.timeout : 2000,
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+    .get(user_endpoint);
+}
 
 export default {
-  authenticate
+  authenticate,
+  getMyData
 };
